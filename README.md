418a1acf108ada6760f6369590351c4f2febe3f5
carlstep <carlstep333@me.com>
2023-07-22 06:35:08 +0700

    meals app with riverpod

---
+++ README.md
@@ -0,0 +1,3 @@
+# state_management_rp
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/state_management_rp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.state_management_rp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
